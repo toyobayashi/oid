aaa6fa1dd9f7eff61b77c79ace4f07bcf028b1ca
toyobayashi <lifenglin314@outlook.com>
2019-09-20 15:01:41 +0800

    1.0.4

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,14 @@
 declare class ObjectId {
-  constructor ();
-  constructor (id: number);
-  constructor (id: string);
-  constructor (id: number[]);
-  constructor (id: Uint8Array);
-  constructor (id: ObjectId);
+  constructor (id?: number | string | number[] | Uint8Array | ObjectId);
 
   static createFromTime (time: number): ObjectId;
   static createFromHexString (hexString: string): ObjectId;
 
-  static isValid (otherId: number): boolean;
-  static isValid (otherId: string): boolean;
-  static isValid (otherId: number[]): boolean;
-  static isValid (otherId: Uint8Array): boolean;
-  static isValid (otherId: ObjectId): boolean;
+  static isValid (otherId: number | string | number[] | Uint8Array | ObjectId): boolean;
 
   static createPk (): ObjectId;
 
-  static generate (): Uint8Array;
-  static generate (time: any): Uint8Array;
+  static generate (time?: number): Uint8Array;
 
   static getInc(): number;
   static fromExtendedJSON(doc: ObjectId.Document): ObjectId;
@@ -28,14 +18,11 @@ declare class ObjectId {
 
   toHexString(): string;
 
-  equals(otherId: string): boolean;
-  equals(otherId: Uint8Array): boolean;
-  equals(otherId: ObjectId): boolean;
+  equals(otherId: string | Uint8Array | ObjectId): boolean;
 
   getTimestamp(): Date;
 
-  toString(): string;
-  toString(format: string): string;
+  toString(format?: string): string;
 
   toJSON(): string;
   toExtendedJSON(): ObjectId.EmptyDocument;
